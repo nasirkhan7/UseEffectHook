c60f455f345a3875cca927e68b6e9f55290094c1
nasirkhan <nasirkhana842@gmail.com>
2023-12-06 02:33:24 -0500

    New project

--- src/useEffect/BasicUseEffect.jsx
+++ src/useEffect/BasicUseEffect.jsx
@@ -3,13 +3,19 @@ import React, { useEffect, useState } from 'react'
 const BasicUseEffect = () => {
     const [test,setTest] = useState(0)
     useEffect(()=>{
-        console.log('Hello world');
-    })
+        if(test === 0){
+            document.title = 'WhatsApp'
+        }
+        else{
+            document.title = `(${test}) new messages`
+            
+        }
+    },[test])
   return (
     <>
-        <div className="container text-center text-capitalize">
+        <div className="container text-capitalize text-center">
             <h1>{test}</h1>
-            <button className="btn btn-dark">
+            <button onClick={()=>setTest(test+1)} className="btn btn-dark">
                 Click
             </button>
         </div>

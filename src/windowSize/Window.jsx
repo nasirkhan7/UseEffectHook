9d91379889ff31c420fa3787fe681ce6f27fe093
nasirkhan <nasirkhana842@gmail.com>
2023-12-07 05:30:05 -0500

    New project

--- src/windowSize/Window.jsx
+++ src/windowSize/Window.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Window = () => {
-    const [size,setSize] = useState(window.innerWidth)
-    window.addEventListener('resize',()=>{
-        setSize(window.innerWidth)
-    })
-  return (
-    <>
+const [size,setSize] = useState(window.innerWidth)
+const handleSize = ()=>{
+    
+}
+window.addEventListener('resize',()=>{
+setSize(window.innerWidth)
+})
+
+useEffect(()=>{
+window.addEventListener('resize',handleSize);
+return ()=>{
+window.removeEventListener('resize',()=>{
+})
+}
+})
+
+return (
+<>
     <div className="container text-center text-capitalize">
         <h1>Window Size : {size}px </h1>
-
     </div>
-    </>
-  )
+</>
+)
 }
